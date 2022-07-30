353fdb598f0bf16434c63de27f45585bae39dd16
Soumita Basu <moon1237879@gmail.com>
2022-07-30 06:54:24 +0530

    Images logo added

--- my-app/src/components/App.jsx
+++ my-app/src/components/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import Header from './header';
 
 function App(){
-    return( <div>
-        Hello
+    return( <div className='main container-fluid p-0'>
+        <Header/>
     </div>
     );
 }
