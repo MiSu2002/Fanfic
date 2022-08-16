e1cbe3d8447c9a978129fe8a78b88456ab741da0
Soumita Basu <moon1237879@gmail.com>
2022-08-17 04:21:25 +0530

    Description added. Responsive title.

--- my-app/src/components/firstBody.jsx
+++ my-app/src/components/firstBody.jsx
@@ -11,7 +11,13 @@ function firstBody(){
             </div>
                 <div className='feature-img justify-content-center d-flex'></div>
             </div>
-            <div className='title1'>Differences Between Us</div>
+            <div className='title1 d-flex justify-content-center'>Differences Between Us</div>
+            <div className='paragraph d-flex justify-content-center text-center'>Can you dive into an endless loop of love and lust? <br></br>How far can you go for their love?</div>
+            {/* <div className='tags d-flex justify-content-center'>
+                <div className='me-5'>BL</div>
+                <div className='ms-5'>romcom</div>
+            </div> */}
+            <div className='description'>You, CEO of Hang Electronics, a successful businesswoman at the age of 25, had never experienced love cause of a busy schedule. You were committed not to falling in love with anyone until you found yourself engaged in an endless loop of love and lust after involving in an affair with two handsome stars from a popular boy band group.</div>
             </section>
         </div>
     
